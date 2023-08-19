e183d1b3e345593bf69020ffe553db22589dd7b9
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-19 14:09:47 +0300

    feat: implement logic & template to login component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { UserService } from '../../user.service';
 
 @Component({
@@ -9,37 +10,54 @@ import { UserService } from '../../user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  user: SocialUser = {
-    provider: '',
-    id: '',
-    email: '',
-    name: '',
-    photoUrl: '',
-    firstName: '',
-    lastName: '',
-    authToken: '',
-    idToken: '',
-    authorizationCode: '',
-    response: '',
-  };
+  loginForm: FormGroup = this.formBuilder.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$'),
+      ],
+    ],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+    rememberMe: [false],
+  });
+
+  user: SocialUser = new SocialUser();
   loggedIn = false;
 
   constructor(
     private authService: SocialAuthService,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private formBuilder: FormBuilder
   ) {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit() {
     this.authService.authState.subscribe(user => {
       this.user = user;
       this.loggedIn = user != null;
-      console.log(this.user);
-
       if (this.loggedIn) {
+        localStorage.setItem('email', user.email);
         this.userService.setUser(this.user);
         this.router.navigate(['/posts']).then(r => r);
       }
     });
+  }
+
+  onLoginSubmit() {
+    const value = this.loginForm.value;
+    if (value) {
+      localStorage.setItem('email', value.email);
+      this.userService.setUser(this.user);
+      this.router.navigate(['/posts']).then(r => r);
+    }
   }
 }
