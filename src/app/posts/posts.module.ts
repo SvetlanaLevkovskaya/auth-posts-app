e3d95c1a2244bbdbc1f8bbf087fea3fdf3b5a52b
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-19 19:15:32 +0300

    feat: add header component

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostsRoutingModule } from './posts-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PostsComponent],
-  imports: [CommonModule, PostsRoutingModule, FormsModule],
+  imports: [CommonModule, PostsRoutingModule, FormsModule, SharedModule],
   providers: [],
 })
 export class PostsModule {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../auth/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private userService: UserService) {}
+
+  get userEmail(): string | undefined {
+    return this.userService.getUser()?.email;
+  }
+
+  logout(): void {
+    this.userService.setLoggedOut();
+  }
+}
