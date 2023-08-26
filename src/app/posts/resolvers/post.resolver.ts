6f9328bbe4e89b75779a886eea2c02baf69de274
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-26 21:13:14 +0300

    fix: implement async/await concept to resolve feature

--- src/app/posts/resolvers/post.resolver.ts
+++ src/app/posts/resolvers/post.resolver.ts
@@ -2,27 +2,25 @@ import { ResolveFn, Router } from '@angular/router';
 import { Post } from '../interfaces/post.interfaces';
 import { inject } from '@angular/core';
 import { PostsService } from '../services/posts.service';
-import { catchError, from, lastValueFrom, of, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
 
-export const postResolver: ResolveFn<Post | undefined> = route => {
+export const postResolver: ResolveFn<
+  Promise<Observable<Post> | undefined>
+> = async route => {
   const postsService = inject(PostsService);
   const router = inject(Router);
 
   const postId = Number(route.paramMap.get('id'));
-  const postPromise = postsService.fetchPostById(postId);
-
-  return from(lastValueFrom(postPromise)).pipe(
-    switchMap(post => {
-      if (post) {
-        return of(post);
-      } else {
-        router.navigate(['/not-found']).then(r => r);
-        return of(undefined);
-      }
-    }),
-    catchError(() => {
-      router.navigate(['/not-found']).then(r => r);
-      return of(undefined);
-    })
-  );
+  try {
+    const post = await postsService.fetchPostById(postId);
+    if (post) {
+      return post;
+    } else {
+      await router.navigate(['/not-found']).then(r => r);
+      return undefined;
+    }
+  } catch (error) {
+    await router.navigate(['/not-found']).then(r => r);
+    return undefined;
+  }
 };
